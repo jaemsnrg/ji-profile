80cedec61311f680ec1f792c2be6c4577997ebef
James <jaemsnrg@gmail.com>
2020-02-10 21:59:01 +0000

    Updates

---
+++ precache-manifest.1c21e43b6fc2dd5d6d6dfac3b93de3a1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98bc471b858dc1fe63a46a9645176faf",
+    "url": "/ji-profile/index.html"
+  },
+  {
+    "revision": "4e496e8f4abafeb2610c",
+    "url": "/ji-profile/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "8dcc0df6058fd08bdb88",
+    "url": "/ji-profile/static/js/2.de39a445.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/ji-profile/static/js/2.de39a445.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e496e8f4abafeb2610c",
+    "url": "/ji-profile/static/js/main.7ee18fee.chunk.js"
+  },
+  {
+    "revision": "e1288c895122f047b2aa",
+    "url": "/ji-profile/static/js/runtime-main.5a14a875.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ji-profile/static/media/logo.5d5d9eef.svg"
+  }
+]);
